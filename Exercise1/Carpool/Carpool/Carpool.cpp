2342121dfe179e1bc03a1193c05cc1d9a1bb4738
D4NW8 <D4nW8@users.noreply.github.com>
2019-10-17 10:40:41 +0200

    Added Print to search function, added try-catch-blocks

--- Exercise1/Carpool/Carpool/Carpool.cpp
+++ Exercise1/Carpool/Carpool/Carpool.cpp
@@ -1,1 +1,58 @@
 #include "Carpool.h"
+
+using namespace std; 
+
+void Carpool::Add(Vehicle const& vehicle) {
+	try {
+		if (FindVehicle(vehicle.GetNumberplate()) == mVehicles.cend()) {
+			throw exception("Add failed: Number already in the Database!");
+		}
+
+		mVehicles.emplace_back(vehicle);
+	}
+	catch (exception const& ex) {
+		cerr << ex.what() << endl;
+		return;
+	}
+	catch (bad_alloc const& ex) {
+		cerr << "memory allocation: " << endl;
+	}
+}
+
+void Carpool::Remove(std::string const& numberplate) {
+	try {
+		auto vehicleToDel = FindVehicle(numberplate);
+		if (vehicleToDel == mVehicles.cend()) {
+			throw exception("Delete failed: the entered numberplate is not existing!");
+		}
+		remove(mVehicles.begin(), mVehicles.end(), *vehicleToDel);
+	}
+	catch (exception const& ex) {
+		cerr << ex.what() << endl;
+		return;
+	}
+	catch (bad_alloc const& ex) {
+		cerr << "memory allocation: " << endl;
+	}
+}
+
+void Carpool::SearchVehicle(std::string const& numberplate) {
+	auto foundVehicle = FindVehicle(numberplate);
+	if (foundVehicle != mVehicles.end()) {
+		cout << "The vehicle with the numberplate :" << numberplate << "is not registered in this carpool! " << endl;
+	}
+	*foundVehicle.Print();
+}
+
+VehicleCItor Carpool::FindVehicle(std::string const& numberplate) const {
+	auto PredNumberP = [numberplate](Vehicle const& vehicle) { //can be deleted after == operator is overloaded!
+		return (numberplate == vehicle.GetNumberPlate());
+	};
+
+	return find_if(mVehicles.cbegin(), --mVehicles.cend(), PredNumberP);
+}
+
+unsigned long Carpool::TotalMileage() const {
+	return 0;
+}
+
--- Exercise1/Carpool/Carpool/Carpool.h
+++ Exercise1/Carpool/Carpool/Carpool.h
@@ -1,3 +1,29 @@
-#pragma once
+#ifndef CARPOOL_H
+#include "Vehicle.h"
+
+#include <iostream>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <exception>
+#include <string>
+#include <vector>
+
+
+typedef std::vector<Vehicle>::const_iterator VehicleCItor;
+
 class Carpool{
+public:
+	void Add(Vehicle const& vehicle);
+	void Remove(std::string const& numberplate);
+	void SearchVehicle(std::string const& numberplate);
+	unsigned long TotalMileage() const;
+
+private:
+	std::vector<Vehicle> mVehicles;
+
+	VehicleCItor FindVehicle(std::string const& numberplate) const;
+
 };
+
+#endif //CARPOOL_H

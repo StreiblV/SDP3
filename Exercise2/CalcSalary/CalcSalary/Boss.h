c04d624f5044f99263fbf0a949395a2c733b3e29
D4NW8 <D4nW8@users.noreply.github.com>
2019-11-03 21:07:28 +0100

    Added first functiondeclarations

--- Exercise2/CalcSalary/CalcSalary/Boss.h
+++ Exercise2/CalcSalary/CalcSalary/Boss.h
@@ -1,5 +1,18 @@
-#pragma once
-class Boss
-{
+#ifndef BOSS_H
+#define BOSS_H
+
+#include <string>
+
+class Boss {
+public:
+	virtual std::string GetType() const override;
+
+	virtual void SetProducedPieces() override;
+	virtual void SetSoldPieces() override;
+	virtual std::size_t GetProdPieces() const override;
+	virtual std::size_t GetSoldPieces() const override;
+	virtual std::string GetNickname() const = 0;
+
 };
 
+#endif //BOSS_H

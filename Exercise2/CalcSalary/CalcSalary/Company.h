b58dabab567b279fd46a62a04803f88ac5613027
StreiblV <viktoria.streibl@sequality.at>
2019-11-03 21:16:17 +0100

    add local variables

--- Exercise2/CalcSalary/CalcSalary/Company.h
+++ Exercise2/CalcSalary/CalcSalary/Company.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include <string>
+#include <list>
+
+#include "Employee.h"
+
 class Company
 {
+
+public:
+
+private:
+	std::string m_name;
+	std::string m_location;
+	std::list<Employee> m_employees;
 };
 

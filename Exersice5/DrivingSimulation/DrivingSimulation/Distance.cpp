c94beac754a9ead3354f25e31cd3ef56c7e640f1
StreiblV <viktoria.streibl@sequality.at>
2019-12-09 20:21:53 +0100

    add current stuff

--- Exersice5/DrivingSimulation/DrivingSimulation/Distance.cpp
+++ Exersice5/DrivingSimulation/DrivingSimulation/Distance.cpp
@@ -2,18 +2,24 @@
 | Workfile : Distance.cpp
 | Description : [ SOURCE ]
 | Name : Daniel Weyrer			PKZ : S1820306044
+		 Viktoria Streibl		PKZ : S1810306013
 | Date : 09.12.2019
 | Remarks : -
 | Revision : 0
 | _______________________________________________________________________ */
 
 #include "Distance.h"
+#include "WindowsDisplay.h"
 
 static const double conversionFactorToKMH = 3.6;
 
 
 void Distance::Update() {
+	WindowsDisplay::SPtr anaDisp = std::make_shared<AnalogDisplay>();
+
 	mDistance = CalcDistance(mCar->GetRPM());
+
+
 }
 
 double Distance::CalcDistance(unsigned int rpm) const {
